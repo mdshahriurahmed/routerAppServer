5c996989b0c5542b5a09d1f89a6ff68ab8ef8819
mdshahriurahmed <shahriurahmedcse@gmail.com>
2026-03-28 12:08:02 +0600

    update6

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tvjrc2r.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://shahriurahmedcse_db_user:cyEljaq6SYPC7qvZ@cluster0.tvjrc2r.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,11 +38,12 @@ const verifyToken = (req, res, next) => {
     console.log("inside verify", req.cookies);
     const token = req.cookies.token
     if (!token) { return res.status(401).send({ message: 'unauthorized access' }) }
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    jwt.verify(token, '9ec6823a06e87c439a79581dafbb3588c2f1305ba5badc9b6b57b22c2a76c96454a478cddfb5c04bf7f1602e4148c4763ea186359e394418b865b51c0b35c8d4', (err, decoded) => {
         if (err) { return res.status(401).send({ message: 'unauthorized access' }) }
         next();
     })
 }
+
 
 
 async function run() {
